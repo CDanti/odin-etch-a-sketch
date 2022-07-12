c577d179e5c88d002d5c19a122b9e468b4c4c1bf
Cdanti <chrisdanti2001@gmail.com>
2022-07-12 14:38:55 -0400

    mmm just started! and having trouble!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-etch-a-sketch
+
+This is my attempt at the etch-a-sketch project for The Odin Project (7/12/2022).
+
---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+window.onload=() => {
+    const container = document.querySelector('#containerGrid');
+    const sketchSquare = document.createElement('div');
+    sketchSquare.classList.add('square');
+    container.appendChild(sketchSquare);
+    container.appendChild(sketchSquare);
+}
